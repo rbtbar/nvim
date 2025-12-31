6148168c7b6ce280ed937b829ceeb82f65345706
Robert <robert@rbsoftware.pl>
2025-12-31 12:45:56 +0100

    auto-reload files (for claude code workflow)

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,20 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Auto-reload files changed externally (Claude Code workflow)
+vim.opt.autoread = true
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.buftype == "" then vim.cmd("checktime") end
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  pattern = "*",
+  callback = function()
+    vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.INFO)
+  end,
+})
